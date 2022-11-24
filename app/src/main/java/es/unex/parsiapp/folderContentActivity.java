bb76c2ab10108a33f395a745475c4658162405a6
Señor Naranja - Javier Mateos - <jamateosb@gmail.com>
2022-11-24 12:31:10 +0100

    PGD03-244 CU07 - IT4 - IMP - Implement Components - AS #DONE #TIME 1d 40m Implementación del caso de uso 7, borrar post de carpeta, Arquitecto Software.

--- app/src/main/java/es/unex/parsiapp/folderContentActivity.java
+++ app/src/main/java/es/unex/parsiapp/folderContentActivity.java
@@ -124,7 +124,17 @@ public class folderContentActivity extends AppCompatActivity {
                 popupFolders.setTitle("Borrar post de carpeta").setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        // Obtencion del ID del post
+                        ImageButton imgButton = (ImageButton) v;
+                        long post_id = (long) imgButton.getTag(R.string.idSaveDb);
+                        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                database.getPostDao().delete(post_id);
+                            }
+                        });
+                        Toast.makeText(folderContentActivity.this, "Se ha borrado con éxito.", Toast.LENGTH_SHORT).show();
+                        showContentFolder();
                     }
                 }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
                     @Override
