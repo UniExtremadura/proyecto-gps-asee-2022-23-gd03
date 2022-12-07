c058066bc2361eb5e7db008b876843bdc32cbdd5
Pablo Rodríguez Martín <rodmarprogrammer@gmail.com>
2022-12-07 23:34:30 +0100

    PGD03-295 CU09 - IT5 - IMP - Test Components - DJ #done #time 2h 39m Realizado parte del test unitario relativo al CU09

---
+++ app/src/test/java/es/unex/parsiapp/ColumnaTest.java
@@ -0,0 +1,22 @@
+package es.unex.parsiapp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import es.unex.parsiapp.model.Columna;
+
+public class ColumnaTest {
+
+    @Test
+    public void setColumnaActual() {
+        Columna columna = new Columna();
+        columna.setNombre("Columna 1");
+        columna.setColumnaActual(true);
+
+        assertTrue("La columna no está marcada como la columna actual.", columna.isColumnaActual());
+    }
+
+}
