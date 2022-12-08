4f9547eab739d99a45f8f90c396f2ff45c1cad8f
Señor Marron - Andres - <agarciavwl@alumnos.unex.es>
2022-12-08 13:41:13 +0100

    PGD03-71 CU03 - IT3 - IMP - Test Components - DJ #done #time 4h 34m Parte del test unitario de Carpeta del caso de uso 03, correspondiente al DJ.

---
+++ app/src/test/java/es/unex/parsiapp/CarpetaTest.java
@@ -0,0 +1,33 @@
+package es.unex.parsiapp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.unex.parsiapp.model.Carpeta;
+import es.unex.parsiapp.model.Post;
+
+public class CarpetaTest {
+
+    private List<Post> listaPosts;
+
+    @Test
+    public void anadirPost() {
+        listaPosts = new ArrayList<Post>();
+
+        Post p1 = new Post(1, "1234", 1);
+
+        listaPosts.add(p1);
+
+        assertTrue("El tamaño de la lista de posts no es el esperado.", listaPosts.size() == 1);
+        assertFalse("La lista de posts está vacía.", listaPosts.isEmpty());
+        assertNotEquals("El primer elemento de la lista es nulo.", listaPosts.get(0), null);
+    }
+}
