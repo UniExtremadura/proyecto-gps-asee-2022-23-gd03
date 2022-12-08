2322119bae4302319564175183474ea594584f6b
Javier Florido <javierfloridocartolano@gmail.com>
2022-12-08 11:14:38 +0100

    PGD03-113 CU15 - IT8 - IMP - Test Components - AS #done #time 6h 26m - Se han implementado los tests unitarios del CU15 (referentes al AS)

---
+++ app/src/test/java/es/unex/parsiapp/model/CarpetaTest.java
@@ -0,0 +1,26 @@
+package es.unex.parsiapp.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarpetaTest {
+
+    private List<Post> listaPosts;
+
+    @Test
+    public void setNombre() {
+        Carpeta carpeta = new Carpeta();
+        String nombreCarpeta = "Carpeta 1";
+        carpeta.setNombre(nombreCarpeta);
+
+        assertEquals("El nombre de la carpeta no coincide.", carpeta.getNombre(), "Carpeta 1");
+    }
+}
