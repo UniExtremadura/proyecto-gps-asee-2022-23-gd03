68323e79adc5e08c450468353ea3eb2303f67eaf
Javier Florido <javierfloridocartolano@gmail.com>
2022-12-08 16:54:34 +0100

    PGD03-247 CU07 - IT4 - IMP - Test Components - DJ #done #time 58m - Se han implementado tests unitarios del CU07 (relativo a DJ)

--- app/src/test/java/es/unex/parsiapp/model/PostTest.java
+++ app/src/test/java/es/unex/parsiapp/model/PostTest.java
@@ -15,4 +15,14 @@ public class PostTest {
         assertEquals("El id de la carpeta no coincide.", post.getIdCarpeta(), 5);
         assertNotNull("El id de la carpeta es nulo.", post.getIdCarpeta());
     }
+
+    @Test
+    public void setIdDb() {
+        Post post = new Post("1");
+        long idDbPost = 5;
+        post.setIdDb(idDbPost);
+
+        assertEquals("El idDb del post no coincide.", post.getIdDb(), 5);
+        assertNotNull("El idDb del post es nulo.", post.getIdDb());
+    }
 }
